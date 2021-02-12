e933b6a0036bcbe4deb5526fe47048d8f7848d6b
Frano Rajič <frano.rajich@gmail.com>
2021-02-12 14:16:49 +0100

    Minor project structure refactor

--- playground/lab1/summarize_runs.py
+++ playground/lab1/summarize_runs.py
@@ -184,7 +184,8 @@ def zipdir(path, ziph):
     # ziph is zipfile handle
     for root, dirs, files in os.walk(path):
         for file in files:
-            ziph.write(os.path.join(root, file), os.path.relpath(os.path.join(root, file), os.path.join(path, '../..')))
+            ziph.write(os.path.join(root, file), os.path.relpath(os.path.join(root, file), os.path.join(path,
+                                                                                                        '../../demo')))
 
 
 zipf = zipfile.ZipFile(results_path + ".zip", 'w', zipfile.ZIP_DEFLATED)
